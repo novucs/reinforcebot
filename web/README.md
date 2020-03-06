77b3e8303869a3a40811bdf27786a8e610cf9dc7
novucs <contact@novucs.net>
2020-03-06 10:17:26 +0000

    Add backend jwt authentication

---
+++ web/README.md
@@ -0,0 +1,15 @@
+# ReinforceBot Web Services
+
+## Prerequisites
+* [docker](https://docs.docker.com/install/)
+* [docker-compose](https://docs.docker.com/compose/install/)
+* A web browser
+
+## QuickStart
+
+```bash
+docker-compose up -d
+docker-compose exec web python manage.py migrate
+```
+
+Open in browser: http://localhost:8080/
--- web/django/web/api.py
+++ web/django/web/api.py
@@ -1,24 +1,9 @@
 from django.contrib.auth import get_user_model
-from rest_framework import (
-    permissions,
-    viewsets,
-)
-from rest_framework.generics import CreateAPIView
+from rest_framework import viewsets
 
-from web.serializers import (
-    CreateUserSerializer,
-    UserSerializer,
-)
+from web.serializers import UserSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
-
-
-class CreateUserView(CreateAPIView):
-    model = get_user_model()
-    permission_classes = [
-        permissions.AllowAny  # Or anon users can't register
-    ]
-    serializer_class = CreateUserSerializer
