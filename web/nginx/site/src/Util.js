7024cbfec04297038caf228dd02963323ede16e0
novucs <contact@novucs.net>
2020-03-16 21:47:18 +0000

    Add server configs

--- web/nginx/site/src/Util.js
+++ web/nginx/site/src/Util.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:8080';
+export const BASE_URL = 'http://reinforcebot.novucs.net';
 
 export function displayErrors(...errors) {
   return errors.flatMap((e) => displayError(e));
