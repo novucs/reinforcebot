9e112b93c19f49fa11bf7ffe107bd024e976fcfd
novucs <contact@novucs.net>
2020-03-28 10:15:17 +0000

    Add plan for Q scheme

--- client/reinforcebot/agent.py
+++ client/reinforcebot/agent.py
@@ -9,32 +9,32 @@ from torch import nn
 
 class ReplayBuffer:
     def __init__(self, observation_space, action_space, max_size=1024):
-        self.d = np.empty((max_size,))
-        self.s = np.empty((max_size, *observation_space.shape))
+        self.o = np.empty((max_size, *observation_space.shape))
         self.a = np.empty((max_size, *action_space.shape))
         self.r = np.empty((max_size,))
-        self.sp = np.empty((max_size, *observation_space.shape))
+        self.n = np.empty((max_size, *observation_space.shape))
+        self.d = np.empty((max_size,))
         self.index = 0
         self.size = 0
         self.max_size = max_size
 
-    def write(self, done, s, a, r, sp):
-        self.d[self.index] = done
-        self.s[self.index] = s
+    def write(self, o, a, r, n, d):
+        self.o[self.index] = o
         self.a[self.index] = a
         self.r[self.index] = r
-        self.sp[self.index] = sp
+        self.n[self.index] = n
+        self.d[self.index] = d
         self.index = (self.index + 1) % self.max_size
         self.size = min(self.size + 1, self.max_size)
 
     def read(self, batch_size=64):
         indices = np.random.randint(0, self.size, size=batch_size)
         return (
-            self.d[indices],
-            self.s[indices],
+            self.o[indices],
             self.a[indices],
             self.r[indices],
-            self.sp[indices],
+            self.n[indices],
+            self.d[indices],
         )
 
 
@@ -64,9 +64,10 @@ class Agent:
         action = np.array([a1, a2]).argmax()
         return action
 
-    def train(self, experience):
-        # Q(s,a)=Q(s,a)-a*(r+max_a(Q(st+1,a))-Q(s,a))
-        pass
+    def train(self, experience, alpha=0.01, gamma=0.99):
+        # Q(s,a)=Q(s,a)-alpha*(r+gamma*max_a(Q(st+1,a))-Q(s,a))
+        o, a, r, n, d = experience
+        # if d == True: max a of any Q for st+1 = 0
 
 
 def main():
@@ -79,13 +80,13 @@ def main():
     replay_buffer = ReplayBuffer(env.observation_space, env.action_space)
     episode_count = 100
     for i in range(episode_count):
-        state = env.reset()
+        observation = env.reset()
         while True:
             env.render()
-            action = agent.act(state)
-            next_state, reward, done, _ = env.step(action)
-            replay_buffer.write(done, state, action, reward, next_state)
-            state = next_state
+            action = agent.act(observation)
+            next_observation, reward, done, _ = env.step(action)
+            replay_buffer.write(observation, action, reward, next_observation, done)
+            observation = next_observation
             if done:
                 experience = replay_buffer.read()
                 agent.train(experience)
