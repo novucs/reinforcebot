9ce5b87dea3d4d6a528ef989143284aebc8ba1f3
novucs <contact@novucs.net>
2020-03-31 20:05:24 +0100

    This is now running

--- client/reinforcebot/agent.py
+++ client/reinforcebot/agent.py
@@ -22,11 +22,11 @@ def copy_params(origin, target, tau=1.0):
 
 
 class ReplayBuffer:
-    def __init__(self, observation_space, action_space, max_size=128000):
-        self.o = np.empty((max_size, *observation_space.shape), dtype=np.float32)
-        self.a = np.empty((max_size, *action_space.shape), dtype=np.int8)
+    def __init__(self, observation_space, action_space, max_size=8192):
+        self.o = np.empty((max_size, *observation_space), dtype=np.float32)
+        self.a = np.empty((max_size,), dtype=np.int8)
         self.r = np.empty((max_size,), dtype=np.float32)
-        self.n = np.empty((max_size, *observation_space.shape), dtype=np.float32)
+        self.n = np.empty((max_size, *observation_space), dtype=np.float32)
         self.d = np.empty((max_size,), dtype=np.bool)
         self.index = 0
         self.size = 0
@@ -41,7 +41,7 @@ class ReplayBuffer:
         self.index = (self.index + 1) % self.max_size
         self.size = min(self.size + 1, self.max_size)
 
-    def read(self, batch_size=8192):
+    def read(self, batch_size=1024):
         indices = np.random.randint(0, self.size, size=batch_size)
         return (
             self.o[indices],
@@ -55,8 +55,8 @@ class ReplayBuffer:
 class Critic(nn.Module):
     def __init__(self, in_dim):
         super(Critic, self).__init__()
-        self.fc1 = nn.Linear(in_dim, 8)
-        self.fc2 = nn.Linear(8, 1)
+        self.fc1 = nn.Linear(in_dim, 200)
+        self.fc2 = nn.Linear(200, 1)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
@@ -68,8 +68,8 @@ class Agent:
     def __init__(self, observation_space, action_space,
                  alpha=0.001, epsilon=0.1, epsilon_decay=1.0, gamma=0.99, tau=0.005):
         self.action_space = action_space
-        self.critic_target = Critic(2 + observation_space.shape[0])
-        self.critic = Critic(2 + observation_space.shape[0])
+        self.critic_target = Critic(self.action_space + np.prod(observation_space))
+        self.critic = Critic(self.action_space + np.prod(observation_space))
         self.critic_criterion = nn.MSELoss()
         self.critic_optimiser = optim.Adam(self.critic.parameters(), lr=alpha)
         copy_params(self.critic, self.critic_target)
@@ -78,27 +78,34 @@ class Agent:
         self.gamma = gamma
         self.tau = tau
 
+    def action(self, id_):
+        """returns one-hot encoded action"""
+        return np.eye(self.action_space)[id_]
+
     def act(self, observation, epsilon=0.1):
         if epsilon > np.random.rand():
-            return self.action_space.sample()
+            return np.random.randint(self.action_space)
 
-        a1 = self.critic_target(torch.from_numpy(np.concatenate((np.eye(2)[0], observation))).float())
-        a2 = self.critic_target(torch.from_numpy(np.concatenate((np.eye(2)[1], observation))).float())
+        observation = observation.reshape(-1)
+        a1 = self.critic_target(torch.from_numpy(np.concatenate((self.action(0), observation))).float())
+        a2 = self.critic_target(torch.from_numpy(np.concatenate((self.action(1), observation))).float())
         action = np.array([a1, a2]).argmax()
         return action
 
     def train(self, experience):
         # Q(s,a)=Q(s,a)-alpha*(r+gamma*max_a(Q(st+1,a))-Q(s,a))
         o, a, r, n, d = experience
+        o = o.reshape(-1, o.shape[0]).swapaxes(0, 1)
+        n = n.reshape(-1, n.shape[0]).swapaxes(0, 1)
         with torch.no_grad():
-            a1 = self.critic_target(torch.from_numpy(np.c_[np.tile(np.eye(2)[0], (len(n), 1)), n]).float())
-            a2 = self.critic_target(torch.from_numpy(np.c_[np.tile(np.eye(2)[1], (len(n), 1)), n]).float())
+            a1 = self.critic_target(torch.from_numpy(np.c_[np.tile(self.action(0), (len(n), 1)), n]).float())
+            a2 = self.critic_target(torch.from_numpy(np.c_[np.tile(self.action(1), (len(n), 1)), n]).float())
             future_q = np.max(np.c_[a1, a2], axis=1)
             future_q[d] = 0
             labels = torch.reshape(torch.from_numpy(r + self.gamma * future_q), [len(a), 1])
 
         self.critic_optimiser.zero_grad()
-        outputs = self.critic(torch.from_numpy(np.c_[np.eye(2)[a], o]).float())
+        outputs = self.critic(torch.from_numpy(np.c_[self.action(a), o]).float())
         loss = self.critic_criterion(outputs, labels)
         loss.backward()
         self.critic_optimiser.step()
@@ -111,21 +118,27 @@ def main():
     args = parser.parse_args()
     env = gym.make(args.env_id)
     env.seed(0)
-    env._max_episode_steps = 200
-    agent = Agent(env.observation_space, env.action_space)
-    replay_buffer = ReplayBuffer(env.observation_space, env.action_space)
+    # env._max_episode_steps = 200
+    observation_space = (2, 80, 80)
+    action_space = 2
+    agent = Agent(observation_space, action_space)
+    replay_buffer = ReplayBuffer(observation_space, action_space)
     episode_count = 10000
     for i in range(episode_count):
+        previous_observation = np.zeros(observation_space[1:])
         observation = env.reset()
+        observation = convert_pong_observation(observation)
         total_steps = 0
         while True:
             total_steps += 1
-            # env.render()
-            action = agent.act(observation)
-            next_observation, reward, done, _ = env.step(action)
-            reward = 0 if not done else 1 if total_steps == 200 else -1
-            replay_buffer.write(observation, action, reward, next_observation, done)
-            observation = next_observation
+            env.render()
+            stacked_observations = np.stack((previous_observation, observation))
+            action = agent.act(stacked_observations)
+            next_observation, reward, done, _ = env.step(2 if action == 0 else 3)
+            next_observation = convert_pong_observation(next_observation)
+            stacked_next_observations = np.stack((observation, next_observation))
+            replay_buffer.write(stacked_observations, action, reward, stacked_next_observations, done)
+            previous_observation, observation = observation, next_observation
             if done:
                 print(f'episode {i}, total steps: {total_steps}')
                 experience = replay_buffer.read()
