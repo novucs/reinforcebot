05d489749cc8e612e81cb18985d40ffbf2bc0597
novucs <contact@novucs.net>
2020-04-11 17:02:29 +0100

    Cleanup window selection

--- client/reinforcebot/main.py
+++ client/reinforcebot/main.py
@@ -1,37 +1,81 @@
 import subprocess
 from datetime import datetime
-from tkinter import messagebox
 
 import cairo
 import gi
 import gym as gym
 import mss
-from PIL import (
-    Image,
-)
-from pynput import (
-    keyboard,
-    mouse,
-)
+from PIL import Image
+from pynput import keyboard, mouse
 
 from reinforcebot import agent
 
 gi.require_version("Gtk", "3.0")
-from gi.repository import (
-    Gtk,
-    Gdk,
-    GLib,
-)
+from gi.repository import Gtk, Gdk, GLib
 
 state = None
 
 
-def on_scroll(x, y, dx, dy):
-    pass
+def limit_bounds(ox, oy, ow, oh):
+    # handle out of bounds negative x/y
+    width = min(ow + ox, ow)
+    height = min(oh + oy, oh)
+    x = max(0, ox)
+    y = max(0, oy)
+
+    # handle out of bounds positive x/y
+    geometry = Gdk.get_default_root_window().get_geometry()
+    width = min(geometry.width - x, width)
+    height = min(geometry.height - y, height)
+
+    if ox != x or oy != y or ow != width or oh != height:
+        print('Warning: The window captured is not entirely visible')
+
+    return x, y, width, height
 
 
-def on_move(x, y):
-    pass
+def select_window(callback):
+    def on_click(mouse_x, mouse_y, button, pressed):
+        if pressed:
+            return
+
+        subprocess.Popen(('gnome-screenshot', '-c', '-w'))
+        geometry = subprocess.check_output(
+            f"wnckprop --xid=$(xdotool getmouselocation --shell | sed -n 's/WINDOW=//p') | sed -n 's/Geometry (x, y, width, height): //p'",
+            shell=True, stderr=subprocess.STDOUT).decode('utf-8')
+        x, y, width, height = map(int, geometry.split(', '))
+        x, y, width, height = limit_bounds(x, y, width, height)
+        callback(x, y, width, height)
+        mouse_listener.stop()
+
+    mouse_listener = mouse.Listener(on_click=lambda *kwargs: on_click(*kwargs))
+    mouse_listener.start()
+
+
+def select_area(callback):
+    subprocess.Popen(('gnome-screenshot', '-c', '-a'))
+
+    def on_click(mouse_x, mouse_y, button, pressed):
+        if pressed:
+            origin[0] = mouse_x
+            origin[1] = mouse_y
+            return
+
+        x = min(origin[0], mouse_x)
+        y = min(origin[1], mouse_y)
+        width = max(origin[0], mouse_x) - x
+        height = max(origin[1], mouse_y) - y
+
+        if width == 0 or height == 0:
+            return
+
+        x, y, width, height = limit_bounds(x, y, width, height)
+        callback(x, y, width, height)
+        mouse_listener.stop()
+
+    origin = [0, 0]  # the position of the cursor when the left mouse button is pressed
+    mouse_listener = mouse.Listener(on_click=lambda *kwargs: on_click(*kwargs))
+    mouse_listener.start()
 
 
 class App:
@@ -40,24 +84,12 @@ class App:
         self.window = window
 
         self.image_on_canvas = None
-        self.select_area_enabled = False
-        self.select_window_enabled = False
-        self.mouse_listener = mouse.Listener(
-            on_move=on_move,
-            on_click=lambda *kwargs: self.on_click(*kwargs),
-            on_scroll=on_scroll,
-        )
-        self.mouse_listener.start()
-
         self.keyboard_listener = keyboard.Listener(
             on_press=lambda *kwargs: self.on_press(*kwargs),
             on_release=lambda *kwargs: self.on_release(*kwargs),
         )
         self.keyboard_listener.start()
-        self.select_window_enabled = False
-        self.select_area_enabled = False
         self.current_coordinates = 0, 0, 0, 0
-        self.area_select_start = 0, 0
         self.last_window_capture = 0
 
     def on_press(self, key):
@@ -66,35 +98,16 @@ class App:
     def on_release(self, key):
         pass
 
-    def on_mfile_item_clicked(self, itemid):
-        if itemid == 'mfile_open':
-            messagebox.showinfo('File', 'You clicked Open menuitem')
-
-        if itemid == 'mfile_quit':
-            self.quit()
-
-    def on_about_clicked(self):
-        messagebox.showinfo('About', 'You clicked About menuitem')
-
     def on_select_window_clicked(self):
-        self.window.hide()
-        self.select_window_enabled = True
+        self.window.iconify()
+        select_window(lambda x, y, width, height: self.capture(x, y, width, height))
 
     def on_select_area_clicked(self):
-        self.window.hide()
-        subprocess.Popen(('gnome-screenshot', '-c', '-a'))
-        self.select_area_enabled = True
+        self.window.iconify()
+        select_area(lambda x, y, width, height: self.capture(x, y, width, height))
 
     def on_record_clicked(self):
         agent.main('reinforcebot-v0')
-
-    def on_coordinates_change(self):
-        ox, oy, ow, oh = self.current_coordinates
-        x, y, w, h = map(lambda c: int(c.get_text()), self.coordinate_variables())
-        w = max(0, w + ox - x)
-        h = max(0, h + oy - y)
-        self.set_displayed_coordinates(x, y, w, h)
-        self.capture(x, y, w, h)
 
     def set_displayed_coordinates(self, x, y, width, height):
         components = self.coordinate_variables()
@@ -133,57 +146,8 @@ class App:
         self.set_image(0, 0, img.width, img.height)
         GLib.idle_add(self.capture_image, x, y, width, height, captured_at)
 
-    def on_click(self, mouse_x, mouse_y, button, pressed):
-        if self.select_window_enabled:
-            if pressed:
-                return
-
-            try:
-                ox, oy, ow, oh = self.parse_window_capture()
-            except ValueError:
-                return
-        elif self.select_area_enabled:
-            if pressed:
-                # todo: disable mouse interactions here, and hide main window.
-                self.area_select_start = (mouse_x, mouse_y)
-                return
-
-            ox = min(self.area_select_start[0], mouse_x)
-            oy = min(self.area_select_start[1], mouse_y)
-            ow = max(self.area_select_start[0], mouse_x) - ox
-            oh = max(self.area_select_start[1], mouse_y) - oy
-
-            if ow == 0 or oh == 0:
-                return
-
-            self.select_area_enabled = False
-        else:
-            return
-
-        # handle out of bounds negative x/y
-        width = min(ow + ox, ow)
-        height = min(oh + oy, oh)
-        x = max(0, ox)
-        y = max(0, oy)
-
-        # handle out of bounds positive x/y
-        width = min(self.builder.get_object('window1').get_screen().get_width() - x, width)
-        height = min(self.builder.get_object('window1').get_screen().get_height() - y, height)
-
-        if ox != x or oy != y or ow != width or oh != height:
-            print('Warning: captured screen is not entirely visible')
-
-        self.capture(x, y, width, height)
-
-    def parse_window_capture(self):
-        subprocess.Popen(('gnome-screenshot', '-c', '-w'))
-        geometry = subprocess.check_output(
-            f"wnckprop --xid=$(xdotool getmouselocation --shell | sed -n 's/WINDOW=//p') | sed -n 's/Geometry (x, y, width, height): //p'",
-            shell=True, stderr=subprocess.STDOUT).decode('utf-8')
-        x, y, width, height = map(int, geometry.split(', '))
-        return x, y, width, height
-
     def capture(self, x, y, width, height):
+        self.window.present()
         print('Captured screen coordinates:', x, y, width, height)
         self.last_window_capture = datetime.now()
         self.current_coordinates = x, y, width, height
