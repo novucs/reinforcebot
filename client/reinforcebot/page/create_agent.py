7dffe2b2bcd129b6e70711e52f9863a9cb317576
novucs <contact@novucs.net>
2020-04-15 00:50:08 +0100

    Start on create agent

---
+++ client/reinforcebot/page/create_agent.py
@@ -0,0 +1,44 @@
+import json
+
+import requests
+from gi.repository import Gtk
+
+from reinforcebot.config import SESSION_FILE
+from reinforcebot.messaging import notify
+
+
+class CreateAgentPage:
+    def __init__(self, app):
+        self.app = app
+        self.builder = app.builder
+        self.builder.get_object('create-agent-button') \
+            .connect("clicked", lambda *_: self.on_create_agent_clicked(), None)
+        self.builder.get_object('cancel-create-agent-button') \
+            .connect("clicked", lambda *_: self.on_cancel_clicked(), None)
+
+        self.window = self.builder.get_object("create-agent")
+        self.window.set_title("ReinforceBot - Create Agent")
+        self.window.connect("destroy", Gtk.main_quit)
+        self.window.set_position(Gtk.WindowPosition.CENTER)
+
+    def present(self):
+        self.window.present()
+
+    def on_create_agent_clicked(self):
+        name = self.builder.get_object('agent-name-entry').get_text()
+        description = self.builder.get_object('agent-description-entry').get_text()
+        response = requests.post(
+            'https://reinforcebot.novucs.net/api/agents/',
+            files={
+                'parameters': (
+                    'parameters.xls',
+                    open('report.xls', 'rb'),
+                    'application/vnd.ms-excel',
+                    {'Expires': '0'},
+                )
+            }
+        )
+
+    def on_cancel_clicked(self):
+        self.window.hide()
+        self.app.router.route('agent_list')
