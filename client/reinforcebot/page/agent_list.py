541f5572cd979e0e248b167c8d113772629a7dc5
novucs <contact@novucs.net>
2020-04-14 17:00:14 +0100

    Add listing pagination

--- client/reinforcebot/page/agent_list.py
+++ client/reinforcebot/page/agent_list.py
@@ -1,3 +1,5 @@
+import math
+import re
 from concurrent.futures.thread import ThreadPoolExecutor
 
 import requests
@@ -13,9 +15,19 @@ class AgentListPage:
         self.builder.get_object('create-button') \
             .connect("clicked", lambda *_: self.on_create_clicked(), None)
 
+        self.builder.get_object('page-start-button') \
+            .connect("clicked", lambda *_: self.change_page(1), None)
+        self.builder.get_object('page-left-button') \
+            .connect("clicked", lambda *_: self.change_page(self.page - 1), None)
+        self.builder.get_object('page-right-button') \
+            .connect("clicked", lambda *_: self.change_page(self.page + 1), None)
+        self.builder.get_object('page-end-button') \
+            .connect("clicked", lambda *_: self.change_page(self.end_page), None)
+
         for i in range(1, 6):
             def clicked_callback(idx):
                 return lambda *_: self.on_agent_detail_clicked(idx)
+
             self.builder.get_object(f'result{i}-button') \
                 .connect("clicked", clicked_callback(i), None)
 
@@ -24,25 +36,59 @@ class AgentListPage:
         self.window.connect("destroy", Gtk.main_quit)
         self.window.set_position(Gtk.WindowPosition.CENTER)
 
+        self.page = 1
+        self.end_page = 1
+        self.page_size = 5
+        self.search = ""
         self.results = None
+        self.authors = None
         self.query_pool = ThreadPoolExecutor(8)
 
     def present(self):
-        self.results = requests.get(API_URL + 'agents/?page=1&page_size=5').json()
-        authors = self.query_pool.map(
-            lambda a: requests.get(API_URL + f'users/{a["author"]}/').json(), self.results['results'])
+        self.fetch()
+        self.render()
+        self.window.present()
 
-        for idx, (agent, author) in enumerate(zip(self.results['results'], authors)):
+    def fetch(self):
+        query = f'agents/?page={self.page}&page_size={self.page_size}'
+        search = re.sub(r'([^\s\w]|_)+', '', self.search)
+        if search:
+            query += f'search="{self.search}"'
+
+        self.results = requests.get(API_URL + query).json()
+        self.authors = self.query_pool.map(
+            lambda a: requests.get(API_URL + f'users/{a["author"]}/').json(), self.results['results'])
+        self.end_page = math.ceil(self.results['count'] / self.page_size)
+
+    def render(self):
+        for idx, (agent, author) in enumerate(zip(self.results['results'], self.authors)):
             idx += 1
             name = f'{author["username"]}/{agent["name"]}'
             description = agent["description"].strip().split('\n')[0]
             if len(description) > 64:
                 description = description[:60] + '...'
 
+            self.builder.get_object(f'result{idx}').show()
+            self.builder.get_object(f'result{idx}-button').show()
             self.builder.get_object(f'result{idx}-name').set_text(name)
             self.builder.get_object(f'result{idx}-description').set_text(description)
 
-        self.window.present()
+        for idx in range(len(self.results['results']) + 1, self.page_size + 1):
+            self.builder.get_object(f'result{idx}').hide()
+            self.builder.get_object(f'result{idx}-button').hide()
+
+        previous_page = '.' if self.results['previous'] is None else str(self.page - 1)
+        next_page = '.' if self.results['next'] is None else str(self.page + 1)
+        self.builder.get_object('page-left-button').set_label(previous_page)
+        self.builder.get_object('page-middle-button').set_label(str(self.page))
+        self.builder.get_object('page-right-button').set_label(next_page)
+
+    def change_page(self, page):
+        if page < 1 or self.end_page < page or self.page == page:
+            return
+        self.page = page
+        self.fetch()
+        self.render()
 
     def on_create_clicked(self):
         print('Create')
